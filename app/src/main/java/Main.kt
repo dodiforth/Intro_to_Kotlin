387be041b4551fe424e31721cf8277458e69b505
dodiforth <dowoniscool@yahoo.com>
2020-11-04 06:56:58 +0100

    Learned about Variables and Types with Tim Buchalka on Udemy course.

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,3 +1,28 @@
 fun main(args: Array<String>) {
     println("Hello World")
+    println("My first Kotlin program")
+
+    var tim: String
+    tim= "Tim Buchalka"
+    println(tim)
+
+    var timsWeeklySalary: Int
+    timsWeeklySalary = 32
+    var timsMonthlySalary = timsWeeklySalary * 4
+    println(timsMonthlySalary)
+
+    println()
+
+    var apples: Int = 6
+    var oranges: Int = 5
+    var fruit: Int = apples- oranges
+    println(fruit)
+
+    println(apples/4)
+
+    println()
+    var weeks: Int = 130
+    var years: Double = weeks / 52.0
+    println(years)
+
 }
