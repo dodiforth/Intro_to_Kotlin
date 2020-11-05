3da7f10ca0f265e13d529b4ee83742c54100fe60
dodiforth <dowoniscool@yahoo.com>
2020-11-05 13:58:08 +0100

    More on variables and types + String Concatenaton and Interpolation (""$, ${}, \$"), and  Val and Var declarations with Tim Buchalka on Udemy course.

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -2,27 +2,31 @@ fun main(args: Array<String>) {
     println("Hello World")
     println("My first Kotlin program")
 
-    var tim: String
-    tim= "Tim Buchalka"
+    val tim = "Tim Buchalka"
     println(tim)
 
-    var timsWeeklySalary: Int
-    timsWeeklySalary = 32
-    var timsMonthlySalary = timsWeeklySalary * 4
-    println(timsMonthlySalary)
+    val timsWeeklySalary = 32
+    val timsMonthlySalary = timsWeeklySalary * 4
+    println("Tim's weekly salary is $timsWeeklySalary")
+    println("Monthly, that comes to $timsMonthlySalary")
 
     println()
 
-    var apples: Int = 6
-    var oranges: Int = 5
-    var fruit: Int = apples- oranges
-    println(fruit)
+    val apples = 6
+    val oranges = 5
+    val fruit: Int = apples- oranges
+    println("$apples apples - $oranges oranges leaves $fruit piece(s) of fruit")
+    println("A quarter of the apple is ${apples/4}")
+    println()
 
-    println(apples/4)
+    //String concatenation and interpolation
 
     println()
-    var weeks: Int = 130
-    var years: Double = weeks / 52.0
-    println(years)
+    val weeks = 234
+    val years: Double = weeks/52.0
+    println("$weeks weeks is $years years")
 
+    println("My nams is $tim")
+
+    println("Cardi B gifted loads of \$\$\$\$ CASH \$\$\$\$ to his boyfriend and apparently it offended her fans")
 }
