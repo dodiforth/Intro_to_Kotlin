2a59ecebb7ff6f5e7c20823f7e503ab5b4d33726
dowoniscool <dowoniscool@yahoo.com>
2020-11-22 14:06:03 +0100

    Last lesson about Kotlin Language from Udemy and Pirple.com. This repository will maybe updated if there's any neeed in the future.

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,37 +1,36 @@
 fun main(args: Array<String>) {
 
-    //for(i in 1..10) {
-    val dracula = VampireKing("Count Dracula")
-    println(dracula)
 
-    dracula.lives = 0
+    val conan = Player("Conan")
+    conan.getLoot(Loot("Invisitility", LootType.POTION, 4.0))
+    conan.getLoot(Loot("Mithril", LootType.ARMOR, 183.0))
+    conan.getLoot(Loot("Ring of Speed", LootType.RING, 25.0))
+    conan.getLoot(Loot("Red Potion", LootType.POTION, 2.0))
+    //conan.getLoot(Loot("Curesed Shield", LootType.ARMOR, -8.0))
+    conan.getLoot(Loot("Brass Ring", LootType.RING, 1.0))
+    conan.getLoot(Loot("Chain Mail", LootType.ARMOR, 4.0))
+    conan.getLoot(Loot("Gold Ring", LootType.RING, 12.0))
+    conan.getLoot(Loot("Gold Ring", LootType.RING, 12.0))
+    conan.getLoot(Loot("Gold Ring", LootType.RING, 12.0))
+    conan.getLoot(Loot("Health Potion", LootType.POTION, 3.0))
+    conan.getLoot(Loot("Silver Ring", LootType.RING, 6.0))
+    conan.getLoot(Loot("Silver Ring", LootType.RING, 6.0))
+    conan.showInventory()
 
-    do {
-        if (dracula.dodges()) {
-            dracula.lives += 1
-            continue;
-        }
-        if (dracula.runAway()) {
-            println("Dracula ran away")
-            break //without 'break' this is an infinite loop
-        } else {
-            dracula.takeDamage(80)
-        }
-    } while (dracula.lives > 0)
-    println("------------------------------------------------")
-    // }
-//    val conan = Player("Conan")
-//    conan.getLoot(Loot("Invisitility", LootType.POTION, 4.0))
-//    conan.getLoot(Loot("Mithril", LootType.ARMOR, 183.0))
-//    conan.getLoot(Loot("Ring of Speed", LootType.RING, 25.0))
-//    conan.getLoot(Loot("Red Potion", LootType.POTION, 2.0))
-//    //conan.getLoot(Loot("Curesed Shield", LootType.ARMOR, -8.0))
-//    conan.getLoot(Loot("Brass Ring", LootType.RING, 1.0))
-//    conan.getLoot(Loot("Chain Mail", LootType.ARMOR, 4.0))
-//    conan.getLoot(Loot("Gold Ring", LootType.RING, 12.0))
-//    conan.getLoot(Loot("Health Potion", LootType.POTION, 3.0))
-//    conan.getLoot(Loot("Silver Ring", LootType.RING, 6.0))
-//    conan.showInventory()
+    conan.dropLoot("Gold Ring")
+    conan.showInventory()
 
-    //This is a comment to test Git update btw my iMac and Dell laptop
+    conan.dropLoot("Silver Ring")
+    conan.showInventory()
+////////Below the codes are checking when an unknown name's used, by attempting to drop something that Conan doesn't have.
+    val dropped = conan.dropLoot("Something not present")
+    println(dropped)
+
+    println(conan.dropLoot("Something else"))
+
+    if (conan.dropLoot("A bit of junk")){
+        println("just dropped")
+    } else {
+        println("You don't have any junk")
+    }
 }
--- app/src/main/java/Player.kt
+++ app/src/main/java/Player.kt
@@ -35,17 +35,45 @@ class Player(val name: String, var level: Int = 1, var lives: Int = 3, var score
         }
     }
 
-    fun dropLoot(name: String): Boolean{
-        println("$name will be dropped")
-        return inventory.removeIf { it.name == name}
+
+
+    //1st Version
+//    fun dropLoot(name: String): Boolean {
+//        for (item in inventory) {
+//            if (item.name == name) {
+//                inventory.remove(item);
+//                return true
+//            }
+//        }
+//        return false
+//    }
+//        println("$name will be dropped")
+//        return inventory.removeIf { it.name == name}
+
+        //->This code doesn't just drop one item per say. So we need a for loop that can only drop one item at once
+
         //So this checks each item in the list and removes it if the name is the same as the name we passed into dropLoot.
         //Now it here(where the it.name) refers to the item that's currently being checked.
         //so if the name of the current item matches Invisibility Potion in our example,
         //then the item gets removed from the list.
         //Now, if any items are removed, it returns true, otherwise it returns false.
-    }
+
     //So this is function overloading, you create another version of the function with different parameters
     //and Kotlin calls the one that matches the argument types.
+
+    //2nd Version
+    fun dropLoot(name: String): Boolean{
+        var result = false
+        for (item in inventory){
+            if (item.name == name){
+                inventory.remove(item)
+                result = true
+                break
+            }
+        }
+        return result
+    }
+
 
     fun showInventory() {
         var total = 0.0
